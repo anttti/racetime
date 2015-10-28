d9f5dfa61ad766ac8ffdaa7d3ce03f49a07daed3
Antti Mattila <antti.mattila@futurice.com>
2015-10-28 20:08:27 +0200

    Add validation

--- js/Input.js
+++ js/Input.js
@@ -1,13 +1,21 @@
 import "../scss/input.scss";
 
 import React from "react";
+import moment from "moment";
+
+const isValidInput = (t, n) => {
+    const time = moment(t, "mm:ss:SS");
+    const name = n.trim();
+
+    return (time.isValid() && name.length > 0);
+}
 
 const Input = props => {
     return (
         <form onSubmit={props.onSubmit}>
             <input className="input" type="text" placeholder="time" onChange={props.onChangeTime} value={props.time} />
             <input className="input" type="text" placeholder="name" onChange={props.onChangeName} value={props.name} />
-            <input className="input__submit" type="submit" onClick={props.onSubmit} />
+            <input className="input__submit" type="submit" onClick={props.onSubmit} disabled={!isValidInput(props.time, props.name)} />
         </form>
     );
 };
