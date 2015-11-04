a4fa5d7a7461a66540dc8f82b16a685d3933ad96
Antti Mattila <antti.mattila@futurice.com>
2015-11-04 20:07:25 +0200

    Format time

--- js/components/Entry.js
+++ js/components/Entry.js
@@ -1,11 +1,12 @@
 import "../../scss/entry.scss";
 
 import React from "react";
+import Time from "../utils/time";
 
 const Entry = ({ time, name }) => {
     return (
         <li className="entry">
-            <span className="entry__time">{time}</span>
+            <span className="entry__time">{Time.toString(time)}</span>
             <span className="entry__name">{name}</span>
         </li>
     );
