cd8bd9631a64d3d1d4eb32964fc0f1f3edc57992
Antti Mattila <antti.mattila@futurice.com>
2015-11-14 16:29:34 +0200

    Add SingleView

--- js/containers/SingleView.jsx
+++ js/containers/SingleView.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import Contest from "components/Contest";
+
 const SingleView = React.createClass({
     render() {
-        return <div>SingleView</div>;
+        const contest = this.props.contest;
+        if (!contest) {
+            return <div>404!</div>;
+        }
+        return <Contest contest={this.props.contest.toJS()} />;
     }
 });
 
 const select = state => {
-    return state;
+    const id = parseInt(state.router.params.id, 10);
+    const contest = state.contests.get("list").find(contest => contest.get("contestid") === id);
+    return { contest };
 };
 
 export default connect(select)(SingleView);
