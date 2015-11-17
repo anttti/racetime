9c5e5ed1a2151903ca92d42c0502d254efad5169
Antti Mattila <antti.mattila@futurice.com>
2015-11-17 10:53:08 +0200

    Change to prod endpoint (todo: use ENV)

--- js/constants/endpoints.js
+++ js/constants/endpoints.js
@@ -1,5 +1,5 @@
-// const ROOT = "http://laptimes.tammerforce.com";
-const ROOT = "http://localhost:3000";
+const ROOT = "http://laptimes.tammerforce.com";
+//const ROOT = "http://localhost:3000";
 
 export default {
     leaderboards: () => `${ROOT}/v1/leaderboard`,
